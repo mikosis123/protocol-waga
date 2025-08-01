e715f26132104b3075398f0990aaa04433db10a6
mikosis123 <mikosis93@gmail.com>
2025-08-01 23:14:08 +0300

    config change

--- components/contract-data/config.tsx
+++ components/contract-data/config.tsx
@@ -2,11 +2,13 @@ import { http, createConfig } from "wagmi";
 import { base, mainnet, baseSepolia } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [mainnet, base, baseSepolia],
+  chains: [baseSepolia],
   connectors: [],
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
-    [baseSepolia.id]: http("https://sepolia.base.org"),
+    // [mainnet.id]: http(),
+    // [base.id]: http(),
+    [baseSepolia.id]: http(
+      "https://base-sepolia.g.alchemy.com/v2/_F1cwvEd3qO1dogdpKRhYZzsnB55S0uy"
+    ),
   },
 });
